1ebf191674060e08f56a87ad73b0048a761fd822
Coruni <mineimc@outlook.com>
2025-07-26 21:26:55 +0800

    🐞 fix: 删除管道白名单

--- src/config/validation.config.ts
+++ src/config/validation.config.ts
@@ -6,7 +6,7 @@ export const validationConfig: ValidationPipeOptions = {
     enableImplicitConversion: true, // 启用隐式转换
   },
   whitelist: true, // 去除未定义的属性
-  forbidNonWhitelisted: true, // 禁止未定义的属性
+  forbidNonWhitelisted: false, // 禁止未定义的属性
   skipMissingProperties: false, // 不跳过缺失的属性
   skipNullProperties: false, // 不跳过 null 属性
   skipUndefinedProperties: false, // 不跳过 undefined 属性
