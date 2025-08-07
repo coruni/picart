a662f5d15ae051950de41c40247960d6de0002b7
amiya <mineimc@outlook.com>
2025-08-08 01:27:32 +0800

    🐞 fix: 修复banner更新

--- src/modules/banner/banner.service.ts
+++ src/modules/banner/banner.service.ts
@@ -4,8 +4,8 @@ import { Repository, Like } from "typeorm";
 import { Banner, BannerStatus } from "./entities/banner.entity";
 import { CreateBannerDto } from "./dto/create-banner.dto";
 import { UpdateBannerDto } from "./dto/update-banner.dto";
-import { PaginationDto } from "src/common/dto/pagination.dto";
-import { ListUtil } from "src/common/utils";
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ListUtil } from 'src/common/utils';
 
 @Injectable()
 export class BannerService {
@@ -21,7 +21,7 @@ export class BannerService {
 
   async findAll(paginationDto?: PaginationDto) {
     const { page = 1, limit = 10 } = paginationDto || {};
-
+    
     const [data, total] = await this.bannerRepository.findAndCount({
       order: {
         sortOrder: "ASC",
@@ -29,7 +29,7 @@ export class BannerService {
       skip: (page - 1) * limit,
       take: limit,
     });
-
+    
     return ListUtil.fromFindAndCount([data, total], page, limit);
   }
 
@@ -43,7 +43,8 @@ export class BannerService {
 
   async update(id: number, updateBannerDto: UpdateBannerDto) {
     const banner = await this.findOne(id);
-    return await this.bannerRepository.update(id, updateBannerDto);
+    Object.assign(banner, updateBannerDto);
+    return await this.bannerRepository.save(banner);
   }
 
   async remove(id: number) {
