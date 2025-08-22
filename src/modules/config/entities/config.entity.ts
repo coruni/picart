6f5c76aa393a80ab3f85c34ed33dfa6ac7fc97d3
amiya <mineimc@outlook.com>
2025-08-22 23:45:45 +0800

    🐞 fix: 修复too long

--- src/modules/config/entities/config.entity.ts
+++ src/modules/config/entities/config.entity.ts
@@ -4,62 +4,63 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
-} from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
+} from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 
-@Entity({ comment: '系统配置表' })
+@Entity({ comment: "系统配置表" })
 export class Config {
-  @ApiProperty({ description: '配置ID' })
+  @ApiProperty({ description: "配置ID" })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty({ description: '键' })
+  @ApiProperty({ description: "键" })
   @Column({
-    comment: '键',
+    comment: "键",
     unique: true,
   })
   key: string;
 
-  @ApiProperty({ description: '值' })
+  @ApiProperty({ description: "值" })
   @Column({
-    comment: '值',
+    comment: "值",
     nullable: true,
+    type: "text",
   })
   value: string;
 
-  @ApiProperty({ description: '描述' })
+  @ApiProperty({ description: "描述" })
   @Column({
-    comment: '描述',
+    comment: "描述",
     nullable: true,
   })
   description: string;
 
-  @ApiProperty({ description: '类型' })
+  @ApiProperty({ description: "类型" })
   @Column({
-    comment: '类型',
-    default: 'string',
+    comment: "类型",
+    default: "string",
   })
   type: string;
 
-  @ApiProperty({ description: '分组' })
+  @ApiProperty({ description: "分组" })
   @Column({
-    comment: '分组',
-    default: 'system',
+    comment: "分组",
+    default: "system",
   })
   group: string;
 
-  @ApiProperty({ description: '是否开放' })
+  @ApiProperty({ description: "是否开放" })
   @Column({
-    comment: '是否开放',
+    comment: "是否开放",
     default: false,
   })
   public: boolean;
 
-  @ApiProperty({ description: '创建时间' })
-  @CreateDateColumn({ comment: '创建时间' })
+  @ApiProperty({ description: "创建时间" })
+  @CreateDateColumn({ comment: "创建时间" })
   createdAt: Date;
 
-  @ApiProperty({ description: '更新时间' })
-  @UpdateDateColumn({ comment: '更新时间' })
+  @ApiProperty({ description: "更新时间" })
+  @UpdateDateColumn({ comment: "更新时间" })
   updatedAt: Date;
 }
