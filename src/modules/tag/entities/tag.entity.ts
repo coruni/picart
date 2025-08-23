13848f47b79c3d35796dddcac70208981898a765
amiya <mineimc@outlook.com>
2025-08-23 15:31:46 +0800

    🐞 fix: 修复标签实体

--- src/modules/tag/entities/tag.entity.ts
+++ src/modules/tag/entities/tag.entity.ts
@@ -4,40 +4,40 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-} from 'typeorm';
+} from "typeorm";
 
-@Entity({ comment: '标签表' })
+@Entity({ comment: "标签表" })
 export class Tag {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 50, unique: true, comment: '标签名称' })
+  @Column({ length: 50, unique: true, comment: "标签名称" })
   name: string;
 
-  @Column({ length: 200, nullable: true, comment: '标签描述' })
+  @Column({ length: 200, nullable: true, comment: "标签描述" })
   description: string;
 
-  @Column({ type: 'text', comment: '标签头像' })
+  @Column({ type: "text", comment: "标签头像", nullable: true })
   avatar: string;
 
-  @Column({ type: 'text', comment: '标签背景' })
+  @Column({ type: "text", comment: "标签背景", nullable: true })
   background: string;
 
-  @Column({ type: 'text', comment: '标签封面' })
+  @Column({ type: "text", comment: "标签封面", nullable: true })
   cover: string;
 
-  @Column({ default: 0, comment: '排序' })
+  @Column({ default: 0, comment: "排序" })
   sort: number;
 
-  @Column({ default: 0, comment: '文章数量' })
+  @Column({ default: 0, comment: "文章数量" })
   articleCount: number;
 
-  @Column('int', { default: 0, comment: '关注数量' })
+  @Column("int", { default: 0, comment: "关注数量" })
   followCount: number;
 
-  @CreateDateColumn({ comment: '创建时间' })
+  @CreateDateColumn({ comment: "创建时间" })
   createdAt: Date;
 
-  @UpdateDateColumn({ comment: '更新时间' })
+  @UpdateDateColumn({ comment: "更新时间" })
   updatedAt: Date;
 }
