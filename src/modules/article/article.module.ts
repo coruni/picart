c5129ce87ff7ce5f0988b73c72edc8c7f7c8ff56
Amiya <mineimc@outlook.com>
2026-01-14 16:07:49 +0800

    🐞 fix: 修复模块导入

--- src/modules/article/article.module.ts
+++ src/modules/article/article.module.ts
@@ -8,6 +8,7 @@ import { Tag } from '../tag/entities/tag.entity';
 import { ArticleLike } from './entities/article-like.entity';
 import { Download } from './entities/download.entity';
 import { BrowseHistory } from './entities/browse-history.entity';
+import { FavoriteItem } from '../favorite/entities/favorite-item.entity';
 import { TagModule } from '../tag/tag.module';
 import { UserModule } from '../user/user.module';
 import { OrderModule } from '../order/order.module';
@@ -17,7 +18,7 @@ import { FavoriteModule } from '../favorite/favorite.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Article, Category, Tag, ArticleLike, Download, BrowseHistory]),
+    TypeOrmModule.forFeature([Article, Category, Tag, ArticleLike, Download, BrowseHistory, FavoriteItem]),
     TagModule,
     UserModule,
     OrderModule,
